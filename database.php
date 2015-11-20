39f4a3e01b29631f22d6994e779e6c2aa295fe18
dhruv1110 <dpate171@nyit.edu>
2015-11-20 11:20:16 -0500

    Added license to file

--- database.php
+++ database.php
@@ -1,4 +1,19 @@
 <?php
+/**********************************************************************
+Copyright 2015 Dhruv Patel
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+***********************************************************************/
 class Database{
 	/* 
 	 * Create variables for credentials to MySQL database
@@ -22,7 +37,7 @@ class Database{
 	// Function to make connection to database
 	public function connect(){
 		if(!$this->con){
-			$this->myconn = @mysqli_connect($this->db_host,$this->db_user,$this->db_pass,$this->db_name);  // mysql_connect() with variables defined at the start of Database class
+			$this->myconn = @mysqli_connect($this->db_host,$this->db_user,$this->db_pass,$this->db_name);  // mysqli_connect() with variables defined at the start of Database class
             if($this->myconn){
                 $this->con = true;
                 return true;
